ea1ac07ec9ec86e1f12c936ee8214e2a2580e3c0
TkachevNikita <ntkachev03@mail.ru>
2024-10-30 17:43:54 +0500

    feat: add register page

--- libs/auth/src/lib/auth/components/register/register.component.ts
+++ libs/auth/src/lib/auth/components/register/register.component.ts
@@ -1,9 +1,45 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { Gender } from '@t-talk/shared';
+import {
+  TuiButton,
+  TuiLink,
+  TuiTextfieldOptionsDirective,
+} from '@taiga-ui/core';
+import { TuiBlock, TuiRadio } from '@taiga-ui/kit';
+import { TuiInputModule } from '@taiga-ui/legacy';
 
 @Component({
   standalone: true,
   selector: 'lib-auth-register',
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    RouterLink,
+    TuiBlock,
+    TuiButton,
+    TuiInputModule,
+    TuiLink,
+    TuiRadio,
+    TuiTextfieldOptionsDirective,
+  ],
   templateUrl: './register.component.html',
+  styleUrl: './register.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  protected readonly registerForm: FormGroup = new FormGroup({
+    firstName: new FormControl<string>('', Validators.required),
+    secondName: new FormControl<string>('', Validators.required),
+    gender: new FormControl<Gender | null>(null, Validators.required),
+    email: new FormControl<string>('', Validators.required),
+    password: new FormControl<string>('', Validators.required),
+  });
+}
