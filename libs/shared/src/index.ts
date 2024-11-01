ec9641b9550746414005b1e05eabb4e32e76eaa8
TkachevNikita <ntkachev03@mail.ru>
2024-11-01 15:12:02 +0500

    feat: add letters validator and error matcher

--- libs/shared/src/index.ts
+++ libs/shared/src/index.ts
@@ -1,2 +1,4 @@
 export * from './lib/shared/enums/gender.enum';
 export * from './lib/shared/interfaces/user.interface';
+export * from './lib/shared/utlis/error-matcher';
+export * from './lib/shared/validators/letters-only.validator';
---
+++ libs/shared/src/lib/shared/utlis/error-matcher.ts
@@ -0,0 +1,34 @@
+import type { AbstractControl } from '@angular/forms';
+import { TuiValidationError } from '@taiga-ui/cdk';
+
+export const errorMatcher = (
+  control: AbstractControl,
+): TuiValidationError | null => {
+  if (control.hasError('required')) {
+    return new TuiValidationError('Это поле обязательно для заполнения');
+  }
+
+  if (control.hasError('min')) {
+    const min = control.getError('min').min;
+
+    return new TuiValidationError(`Минимальное значение: ${min}`);
+  }
+
+  if (control.hasError('max')) {
+    const max = control.getError('max').max;
+
+    return new TuiValidationError(`Максимальное значение: ${max}`);
+  }
+
+  if (control.hasError('maxLength')) {
+    const maxLength = control.getError('maxlength').requiredLength;
+
+    return new TuiValidationError(`Максимальная длина: ${maxLength} символов`);
+  }
+
+  if (control.hasError('lettersOnly')) {
+    return new TuiValidationError('Поле может содержать только буквы');
+  }
+
+  return null;
+};
