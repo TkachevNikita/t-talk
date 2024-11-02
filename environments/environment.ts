f2e78ca9d45a1c5951e75fbd2b237f098e7398c2
envelope <106300938+TkachevNikita@users.noreply.github.com>
2024-11-02 13:36:02 +0500

    fix: change get env logic

--- environments/environment.ts
+++ environments/environment.ts
@@ -1,11 +1,8 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
-export const environment = {
-  apiKey: process.env['API_KEY'] || '',
-  authDomain: process.env['AUTH_DOMAIN'] || '',
-  projectId: process.env['PROJECT_ID'] || '',
-  storageBucket: process.env['STORAGE_BUCKET'] || '',
-  messagingSenderId: process.env['MESSAGING_SENDER_ID'] || '',
-  appId: process.env['APP_ID'] || '',
+export const enivronment = {
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
 };
