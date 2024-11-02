03e47a12ea16e903ae6474b878247013d041bea2
envelope <106300938+TkachevNikita@users.noreply.github.com>
2024-11-02 13:25:33 +0500

    fix: replace env

--- environments/environment.ts
+++ environments/environment.ts
@@ -1,11 +1,11 @@
-import * as dotenv from 'dotenv';
+import dotenv from 'dotenv';
 dotenv.config();
 
 export const environment = {
-  apiKey: process.env.API_KEY || '',
-  authDomain: process.env.AUTH_DOMAIN || '',
-  projectId: process.env.PROJECT_ID || '',
-  storageBucket: process.env.STORAGE_BUCKET || '',
-  messagingSenderId: process.env.MESSAGING_SENDER_ID || '',
-  appId: process.env.APP_ID || '',
+  apiKey: process.env['API_KEY'] || '',
+  authDomain: process.env['AUTH_DOMAIN'] || '',
+  projectId: process.env['PROJECT_ID'] || '',
+  storageBucket: process.env['STORAGE_BUCKET'] || '',
+  messagingSenderId: process.env['MESSAGING_SENDER_ID'] || '',
+  appId: process.env['APP_ID'] || '',
 };
