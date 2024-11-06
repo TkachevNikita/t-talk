f64facaf3cec2a43766eea86e17eaeb7cb0ed1f0
TkachevNikita <ntkachev03@mail.ru>
2024-11-06 16:14:44 +0500

    WIP

--- environments/environment.ts
+++ environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
-  apiKey: "AIzaSyA6qQmg_mHsuQ_Ia5VW2bvuYthaIPa3K6w",
-  authDomain: "t-talk-23d6c.firebaseapp.com",
-  projectId: "t-talk-23d6c",
-  storageBucket: "t-talk-23d6c.firebasestorage.app",
-  messagingSenderId: "334325848643",
-  appId: "1:334325848643:web:4a95626e81b7a9a16e52f6"
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
 };
